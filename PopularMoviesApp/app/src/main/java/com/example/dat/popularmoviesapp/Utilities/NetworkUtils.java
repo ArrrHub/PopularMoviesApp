11aed014b669d5b0c857e75d69f9197cd0b56829
ArrrHub <ArrrHub@gmail.com>
2017-02-10 01:00:00 +0100

    Network Utils

--- PopularMoviesApp/app/src/main/java/com/example/dat/popularmoviesapp/Utilities/NetworkUtils.java
+++ PopularMoviesApp/app/src/main/java/com/example/dat/popularmoviesapp/Utilities/NetworkUtils.java
@@ -1,8 +1,28 @@
 package com.example.dat.popularmoviesapp.Utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 /**
  * Created by dat on 2017-02-10.
  */
 
-public class NetworkUtils {
+public final class NetworkUtils {
+    public static String getResponse(URL url) throws IOException
+    {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try{
+
+        }
+        finally {
+
+        }
+    }
+
 }
